d1766abb0cc455b81775faf47030256637be0862
Tolegen Timur <demigodkz@gmail.com>
2022-12-08 19:58:54 +0600

    fix: correct router

--- src/widgets/Routes/Routes.tsx
+++ src/widgets/Routes/Routes.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
+import { Routes, Route, HashRouter as Router } from 'react-router-dom';
 
 import Layout from 'widgets/Layout';
 import MainPage from 'pages/MainPage';
